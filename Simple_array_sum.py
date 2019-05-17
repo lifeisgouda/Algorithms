3c77f9082d6fac13ccc6d2e7f029d653db7d499c
gouda <dahaejeon@gmail.com>
2019-05-17 10:50:11 +0900

    Create Simple_array_sum.py

---
+++ Simple_array_sum.py
@@ -0,0 +1,10 @@
+# https://www.hackerrank.com/challenges/simple-array-sum/
+
+def simpleArraySum(ar):
+    return sum(ar)
+
+def simpleArraySum(ar):
+    sum = 0
+    for i in ar:
+        sum += i
+    return sum
