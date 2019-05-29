b5e3ae488176dae20b5fe64ae9a14970a6194f0b
gouda <dahaejeon@gmail.com>
2019-05-29 11:25:16 +0900

    Update Mini-Max_Sum.py

--- Mini-Max_Sum.py
+++ Mini-Max_Sum.py
@@ -1,7 +1,12 @@
 # https://www.hackerrank.com/challenges/mini-max-sum/problem
 
+# 1
 def miniMaxSum(arr):
     arr.sort()
     total = sum(arr)
     print((total - arr[-1]), (total - arr[0]))
+    
+# 2
+def minMaxSum(arr):
+    print(sum(arr) - max(arr), sum(arr) - min(arr))
     
