2b8c0d9905b58c4ea5b037dbea28104590ad6f04
gouda <dahaejeon@gmail.com>
2019-06-02 15:03:12 +0900

    Create Chocolate_in_Box.py

---
+++ Chocolate_in_Box.py
@@ -0,0 +1,12 @@
+# https://www.hackerrank.com/challenges/chocolate-in-box/problem
+
+def chocolateInBox(arr):
+    N = 0
+    X = 0
+    for a in arr:
+        xor ^= a
+    for a in arr:
+        if a^xor < a:
+            N += 1
+    
+    return N
